6ee509cf9ed2a20ec2b8899f3330f21d89cf78d9
CharlieHii <charlie_chaper@hotmail.com>
2024-07-09 00:17:33 +0700

    style: add tailwind style on input of user profile form

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "color-gray-400": "#D6D9E4",
+        "color-gray-900": "#2A2E3F",
+      },
+    },
   },
   plugins: [require("daisyui")],
 };
