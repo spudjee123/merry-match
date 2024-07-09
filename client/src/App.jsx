6091c76c7450f682df494851c370251d9ec79689
CharlieHii <charlie_chaper@hotmail.com>
2024-07-09 16:36:05 +0700

    rebase

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,21 @@
 import "./App.css";
 import Nav from "./nav";
-import { BrowserRouter,Route,Routes,useParams } from 'react-router-dom'
+import EditProfilePage from "./pages/user-profile-management/edit-profile-page.jsx";
+import { BrowserRouter, Route, Routes, useParams } from "react-router-dom";
 
 function App() {
-  return <>
-   <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Nav/>}/>
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Nav />} />
+          <Route path="/user/:userId/edit" element={<EditProfilePage />} />
 
-        {/* <Route path="/page" element={<Page />} /> */}
-      </Routes>
+          {/* <Route path="/page" element={<Page />} /> */}
+        </Routes>
       </BrowserRouter>
-  </>;
+    </>
+  );
 }
 
 export default App;
---
+++ client/src/pages/user-profile-management/edit-profile-page.jsx
@@ -0,0 +1,11 @@
+import EditProfileForm from "../../components/user-profile-management/edit-profile-form.jsx";
+
+function EditProfilePage() {
+  return (
+    <section className="px-4 py-10">
+      <EditProfileForm />
+    </section>
+  );
+}
+
+export default EditProfilePage;
