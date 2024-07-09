6cc152fd237a81c6096273c3d4954a3952b0f4db
Paktakraw <tresarpnat27@gmail.com>
2024-07-09 15:53:39 +0700

    feat create ui login,footer,matching page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,21 @@
 import "./App.css";
 import Nav from "./nav";
-import { BrowserRouter,Route,Routes,useParams } from 'react-router-dom'
+import { BrowserRouter, Route, Routes, useParams } from "react-router-dom";
+import Footer from "./components/Footer";
+import Login from "./components/Login";
+import MatchingPage from "./components/MatchingPage";
 
 function App() {
-  return <>
-   <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Nav/>}/>
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Nav />} />
 
-        {/* <Route path="/page" element={<Page />} /> */}
-      </Routes>
+          {/* <Route path="/page" element={<Page />} /> */}
+          <MatchingPage />
+        </Routes>
       </BrowserRouter>
-  </>;
+    </>
+  );
 }
-
-export default App;
