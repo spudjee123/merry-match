cdaa9cb1672e9310316c94273d6ab876adbfeeb8
CharlieHii <charlie_chaper@hotmail.com>
2024-07-10 18:18:02 +0700

    refactor: add tailwind config library

--- client/src/components/user-profile-management/edit-profile-form.jsx
+++ client/src/components/user-profile-management/edit-profile-form.jsx
@@ -22,7 +22,7 @@ function EditProfileForm() {
     );
   }, [location]);
   const inputClassName =
-    "h-12 p-3 pr-4 gap-2 rounded-lg border border-color-gray-400 text-color-gray-900 bg-white";
+    "h-12 p-3 pr-4 gap-2 rounded-lg border border-gray-400 text-gray-900 bg-white";
   const formGroupClassName = "flex flex-col gap-1 w-full";
   const formGroupRowClassName =
     "flex flex-col gap-6 lg:flex-row-reverse lg:gap-6";
@@ -32,13 +32,13 @@ function EditProfileForm() {
       <form className="gap-10 lg:gap-20 flex flex-col">
         <section className=" flex justify-between">
           <article>
-            <h1 className=" text-sm font-semibold leading-[21px] text-color-beige-700 mb-2">
+            <h1 className=" text-sm font-semibold leading-[21px] text-beige-700 mb-2">
               PROFILE
             </h1>
-            <p className=" text-color-purple-500 text-[32px] leading-10 font-bold lg:text-[46px] lg:font-extrabold lg:leading-[57.5px]">
+            <p className=" text-purple-500 text-[32px] leading-10 font-bold lg:text-[46px] lg:font-extrabold lg:leading-[57.5px]">
               Let’s make profile
             </p>
-            <p className=" text-color-purple-500 text-[32px] leading-10 font-bold lg:text-[46px] lg:font-extrabold lg:leading-[57.5px]">
+            <p className=" text-purple-500 text-[32px] leading-10 font-bold lg:text-[46px] lg:font-extrabold lg:leading-[57.5px]">
               to let others know you
             </p>
           </article>
@@ -46,7 +46,7 @@ function EditProfileForm() {
             <button
               type="button"
               id="preview-btn"
-              className=" h-12 px-6 py-3 bg-color-red-100 text-color-red-600 font-bold leading-6 text-center rounded-[99px] drop-shadow-secondary"
+              className=" h-12 px-6 py-3 bg-red-100 text-red-600 font-bold leading-6 text-center rounded-[99px] drop-shadow-secondary"
               onClick={() => document.getElementById("preview").showModal()}
             >
               Preview Profile
@@ -54,7 +54,7 @@ function EditProfileForm() {
             <button
               type="submit"
               id="update-btn"
-              className=" h-12 px-6 py-3 bg-color-red-500 text-white font-bold leading-6 text-center rounded-[99px] drop-shadow-primary"
+              className=" h-12 px-6 py-3 bg-red-500 text-white font-bold leading-6 text-center rounded-[99px] drop-shadow-primary"
             >
               Update Profile
             </button>
@@ -71,7 +71,7 @@ function EditProfileForm() {
                 <label htmlFor="dateOfBirth">Date of birth</label>
                 <input
                   id="dateOfBirth"
-                  className="h-12 p-3 pr-4 gap-2 rounded-lg border border-color-gray-400 bg-white text-color-gray-900"
+                  className="h-12 p-3 pr-4 gap-2 rounded-lg border border-gray-400 bg-white text-gray-900"
                   type="date"
                   required
                 />
@@ -119,13 +119,13 @@ function EditProfileForm() {
 
             <div className={formGroupRowClassName}>
               <div className={formGroupClassName}>
-                <label htmlFor="email" className="text-color-gray-600">
+                <label htmlFor="email" className="text-gray-600">
                   Email
                 </label>
                 <input
                   id="email"
                   className={
-                    "h-12 p-3 pr-4 gap-2 rounded-lg border border-color-gray-400 text-color-gray-900 bg-color-gray-200"
+                    "h-12 p-3 pr-4 gap-2 rounded-lg border border-gray-400 text-gray-900 bg-gray-200"
                   }
                   type="email"
                   disabled
@@ -197,11 +197,11 @@ function EditProfileForm() {
             <div className={formGroupClassName + " lg:col-span-full"}>
               <label htmlFor="hobby">Hobbies / Interests (Maximum 10):</label>
 
-              <div className=" flex flex-wrap p-3 pr-4 gap-2 rounded-lg border border-color-gray-400 bg-white text-color-gray-900">
+              <div className=" flex flex-wrap p-3 pr-4 gap-2 rounded-lg border border-gray-400 bg-white text-gray-900">
                 {hobbiesList.map((item, index) => {
                   return (
                     <div
-                      className=" flex h-full bg-color-purple-100 px-2 py-1 rounded-md text-color-purple-600 gap-2"
+                      className=" flex h-full bg-purple-100 px-2 py-1 rounded-md text-purple-600 gap-2"
                       key={index}
                     >
                       <p>{item}</p>
@@ -244,7 +244,7 @@ function EditProfileForm() {
               </label>
               <textarea
                 id="aboutMe"
-                className="p-3 pr-4 gap-2 rounded-lg border border-color-gray-400 bg-white text-color-gray-900"
+                className="p-3 pr-4 gap-2 rounded-lg border border-gray-400 bg-white text-gray-900"
                 rows="4"
                 maxLength="150"
                 required
@@ -254,18 +254,16 @@ function EditProfileForm() {
         </section>
         <section>
           <article className=" flex flex-col gap-1 mb-6">
-            <h3 className=" text-color-purple-500 text-2xl leading-[30px] font-bold">
+            <h3 className=" text-purple-500 text-2xl leading-[30px] font-bold">
               Profile pictures
             </h3>
-            <p className=" text-color-gray-800 leading-6">
-              Upload at least 2 photos
-            </p>
+            <p className=" text-gray-800 leading-6">Upload at least 2 photos</p>
           </article>
           <div className=" flex flex-wrap gap-2 lg:gap-6 lg:justify-center">
             {images.map((item, index) => (
               <div
                 key={index}
-                className=" w-[167px] h-[167px] bg-color-gray-200 rounded-2xl"
+                className=" w-[167px] h-[167px] bg-gray-200 rounded-2xl"
               >
                 {item ? (
                   <div className="relative w-full h-full">
@@ -282,14 +280,14 @@ function EditProfileForm() {
                         newImages.push("");
                         setImages(newImages);
                       }}
-                      className="absolute top-[-4px] right-[-4px] bg-color-red-utility w-6 h-6 rounded-full text-white"
+                      className="absolute top-[-4px] right-[-4px] bg-red-utility w-6 h-6 rounded-full text-white"
                     >
                       X
                     </button>
                   </div>
                 ) : (
                   <div className="relative w-full h-full">
-                    <div className="absolute top-0 left-0 w-full h-full flex flex-col text-center items-center justify-center text-sm text-color-purple-600 leading-[21px] font-medium gap-2 rounded-2xl">
+                    <div className="absolute top-0 left-0 w-full h-full flex flex-col text-center items-center justify-center text-sm text-purple-600 leading-[21px] font-medium gap-2 rounded-2xl">
                       <p>+</p>
                       <p>Upload photo</p>
                     </div>
@@ -312,14 +310,14 @@ function EditProfileForm() {
           <button
             type="button"
             id="preview-btn"
-            className=" h-12 px-6 py-3 bg-color-red-100 text-color-red-600 font-bold leading-6 text-center rounded-[99px] drop-shadow-secondary"
+            className=" h-12 px-6 py-3 bg-red-100 text-red-600 font-bold leading-6 text-center rounded-[99px] drop-shadow-secondary"
           >
             Preview Profile
           </button>
           <button
             type="submit"
             id="update-btn"
-            className=" h-12 px-6 py-3 bg-color-red-500 text-white font-bold leading-6 text-center rounded-[99px] drop-shadow-primary"
+            className=" h-12 px-6 py-3 bg-red-500 text-white font-bold leading-6 text-center rounded-[99px] drop-shadow-primary"
           >
             Update Profile
           </button>
@@ -328,7 +326,7 @@ function EditProfileForm() {
           <button
             type="button"
             id="delete-profile-btn"
-            className=" text-color-gray-700 font-bold leading-6"
+            className=" text-gray-700 font-bold leading-6"
             onClick={() => document.getElementById("delete").showModal()}
           >
             delete account
@@ -351,20 +349,20 @@ function EditProfileForm() {
               </form>
             </div>
             <div className=" p-4 lg:p-6 flex flex-col gap-6">
-              <p className=" text-color-gray-700 leading-6">
+              <p className=" text-gray-700 leading-6">
                 Do you sure to delete account?
               </p>
               <div className=" flex flex-col lg:flex-row gap-4">
                 <button
                   id="confirm-delete-btn"
-                  className=" bg-color-red-100 px-6 py-3 max-lg:w-full rounded-[99px] text-color-red-600 leading-6 font-bold drop-shadow-secondary"
+                  className=" bg-red-100 px-6 py-3 max-lg:w-full rounded-[99px] text-red-600 leading-6 font-bold drop-shadow-secondary"
                 >
                   Yes, I want to delete
                 </button>
                 <form method="dialog">
                   <button
                     id="denied-delete-btn"
-                    className=" bg-color-red-500 px-6 py-3 max-lg:w-full rounded-[99px] text-white leading-6 font-bold drop-shadow-primary"
+                    className=" bg-red-500 px-6 py-3 max-lg:w-full rounded-[99px] text-white leading-6 font-bold drop-shadow-primary"
                   >
                     No, I don’t
                   </button>
@@ -386,7 +384,7 @@ function EditProfileForm() {
             </form>
             <div className=" flex justify-between">
               <div></div>
-              <div className=" flex flex-col items-end text-color-gray-900">
+              <div className=" flex flex-col items-end text-gray-900">
                 <article>
                   <h1 className=" text-5xl leading-[57.5px]">
                     {"John Snow"} <span>{26}</span>
