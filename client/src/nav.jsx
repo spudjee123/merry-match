5bcc2f304fe0b4f6e3cf114ea2e23908d48c3cd0
NoonMira <163841273+NoonMira@users.noreply.github.com>
2024-07-11 13:50:27 +0700

    feat: add api login

--- client/src/nav.jsx
+++ client/src/nav.jsx
@@ -6,9 +6,9 @@ import hamburger from './assets/images/hamburger.png'
 
 function Nav() {
   return (
-    <div className="navbar bg-white md:pl-[120px] md:pr-[120px] ">
+    <div className="navbar bg-white md:pl-[100px] md:pr-[100px] ">
     <div className="flex-1 px--18 lg:flex-none lg:[calc(100%-320px)]">
-      <a ><img src={logo} alt="logo" className='w-[132px]:h-[38px] '/></a>
+      <a ><img src={logo} alt="logo" className='w-[132px]:h-[38px] md:w-[180px] md:h-[]'/></a>
     </div>
     <div className="flex flex-1 justify-end px-2">
       <div className="flex items-stretch">
@@ -37,9 +37,9 @@ function Nav() {
       <div className='flex flex-rows flex-wrap w-full justify-between place-content-center'>
         
       <div className='hidden md:flex space-x-6 content-start gap-6 '>
-      <Link to="/" className='text-red-800 content-center '> Why Merry Match?</Link>
-      <Link to="/" className='text-red-800 content-center'> How to Merry</Link>
-      <Link to="/" className='content-center'> <button className='w-[129px] h-[48px] rounded-[99px] bg-red-500 text-white content-center'>Login</button> </Link>
+      <Link to="/" className='text-red-800 content-center md:text-[12px] lg:text-[18px]'> Why Merry Match?</Link>
+      <Link to="/" className='text-red-800 content-center md:text-[12px] lg:text-[18px]'> How to Merry</Link>
+      <Link to="/" className='content-center'> <button className='lg:w-[139px] lg:h-[52px] lg:text-[18px] rounded-[99px] bg-red-500 text-white content-center md:w-[112px] md:h-[48px]'>Login</button> </Link>
       </div>
       </div> 
     </nav>
