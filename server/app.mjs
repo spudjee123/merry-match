92e254592aea63b494f9eb68106fdf4246ff8ddb
NoonMira <163841273+NoonMira@users.noreply.github.com>
2024-07-11 15:04:39 +0700

    fix: rebase

--- server/app.mjs
+++ server/app.mjs
@@ -1,12 +1,14 @@
 import express from "express";
 import connectionPool from "./src/utils/db.mjs";
 import registerRouter from "../server/src/routes/register.mjs";
+import loginRouter from '../server/src/routes/login.mjs'
 
 const app = express();
 const port = 4001;
 
 app.use(express.json());
 app.use("/register", registerRouter);
+app.use("/login", loginRouter);
 
 app.get("/test", (req, res) => {
   return res.json("Server API is working 🚀");
