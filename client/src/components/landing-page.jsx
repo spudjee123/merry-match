2eb82d596e11e51dec1882e6337aca7fccbf23a6
CharlieHii <charlie_chaper@hotmail.com>
2024-07-15 01:36:30 +0700

    style: update landing page styling

--- client/src/components/landing-page.jsx
+++ client/src/components/landing-page.jsx
@@ -14,12 +14,12 @@ import Footer from "../components/Footer";
 
 export function LandingPage() {
   return (
-    <div className="bg-[#160404] lg:h-[2700px] max-lg:h-[4000px] pt-[180px] overflow-x-hidden">
+    <div className="bg-[#160404] pt-[180px] overflow-x-hidden">
       <Nav />
       {/* Section 1 */}
-      <div className="relative flex flex-col items-center">
+      <div className="relative flex flex-col lg:flex-row-reverse lg:gap-6 lg:justify-center items-center">
         {/* Background Circles and Images */}
-        <div className="absolute inset-0 max-lg:hidden">
+        <div className="absolute max-lg:hidden">
           <div className="w-[67px] h-[67px] bg-[#411032] rounded-full absolute left-[-10px] top-[120px]" />
           <div className="w-[60px] h-[60px] bg-[#32000E] rounded-full absolute right-[100px] top-[379px]" />
           <div className="w-2 h-2 bg-yellow-900 rounded-full absolute right-[250px] top-[481px]" />
@@ -33,29 +33,15 @@ export function LandingPage() {
         </div>
 
         {/* Main Image */}
-        <div className="absolute right-[300px] top-[-144px] w-[357px] h-[500px]">
+        <div className="relative h-[209px] w-screen lg:w-[357px] lg:h-[758px] z-10">
           <img
-            className="absolute left-[200px] top-0 w-[286px] h-[500px] max-lg:left-[250px] max-lg:top-[80px] max-lg:w-[190px] max-lg:h-[300px]"
+            className="absolute w-[217.77px] h-[305px] lg:w-[357px] lg:h-[500px] lg:top-[-144px] max-lg:top-[-96px] max-lg:left-[-30px] z-10"
             src={female1}
-          />
-          <div className="absolute left-[209px] top-[398px] w-[148px] h-[41px]">
-            <div className="absolute left-[320px] top-[40px] w-[148px] h-[41px] bg-[#64001D] rounded-tl-3xl rounded-bl-3xl rounded-br-3xl rotate-180 origin-top-left max-lg:left-[280px] max-lg:top-[-50px]" />
-            <div className="absolute left-[170px] top-[10px] w-[148px] text-center text-white text-xs leading-[18px] max-lg:left-[130px] max-lg:top-[-80px]">
-              Hi! Nice to meet you
-            </div>
-          </div>
-        </div>
-
-        {/* Secondary Image */}
-        <div className="absolute left-[160px] top-[258px] w-[357px] h-[500px]">
-          <img
-            className="absolute left-[-50px] w-[500px] h-[500px] max-lg:left-[40px] max-lg:top-[430px] max-lg:w-[270px] max-lg:h-[350px] z-10"
-            src={female2}
           />
         </div>
 
         {/* Main Content */}
-        <div className="flex flex-col items-center justify-center gap-6 mt-40 text-center text-white max-lg:mt-[300px]">
+        <div className="flex flex-col items-center justify-center gap-6 lg:mt-[175px] lg:mb-[223px] text-center text-white relative z-10">
           <h1 className="text-6xl font-black leading-[69px] font-['Nunito']">
             Make the <br /> first ‘Merry’
           </h1>
@@ -68,14 +54,21 @@ export function LandingPage() {
             Start matching!
           </button>
         </div>
+        {/* Secondary Image */}
+        <div className="w-screen lg:w-[357px] h-[376.75px] lg:h-[758px] relative">
+          <img
+            className=" h-[376.75px] lg:h-[500px] absolute lg:bottom-0 max-lg:right-[-56px]"
+            src={female2}
+          />
+        </div>
       </div>
 
       {/* Section 2 */}
       <section
         id="section-two"
-        className="relative flex flex-col items-center bg-[#160404] w-screen h-screen top-[100px] lg max-lg:top-[100px]"
+        className=" relative z-10 flex flex-col lg:flex-row lg:justify-center gap-[82px] lg:gap-[25px] lg:pb-20 pt-10 lg:pt-[105px] items-center bg-[#160404]"
       >
-        <div className="absolute left-[100px] top-[300px] w-[500px] max-lg:left-[30px] max-lg:top-[400px] max-lg:w-[330px]">
+        <div className="w-[500px] max-lg:w-[330px]">
           <h2 className="text-pink-400 text-[46px] font-extrabold leading-[57.50px] font-['Nunito']">
             Why Merry <br className="lg:hidden" /> Match?
           </h2>
@@ -94,15 +87,15 @@ export function LandingPage() {
             interested.
           </p>
         </div>
-        <div className="absolute right-[100px] top-[275px] max-lg:left-[10px] max-lg:top-[900px] max-lg:w-[500px] max-lg:h-[500px]">
-          <img className="max-lg:w-[350px]" src={content} />
+        <div className="">
+          <img className="w-full" src={content} />
         </div>
       </section>
 
       {/* Section 3 */}
       <section
         id="section-three"
-        className="relative flex flex-col items-center bg-[#160404] px-40 pt-20 pb-[88px] top-[-100px] max-lg:top-[500px]"
+        className="flex flex-col items-center bg-[#160404] pt-20 pb-[88px] lg:pt-[196.38px] lg:pb-[81px]"
       >
         <h2 className="w-[300px] text-center text-pink-400 text-[46px] font-extrabold leading-[57.50px] font-['Nunito']">
           How to Merry
@@ -135,21 +128,29 @@ export function LandingPage() {
       </section>
 
       {/* Section 4 */}
-      <section className=" bg-[#160404] h-[500px] max-lg:relative max-lg:top-[500px]">
-        <div className="relative flex justify-center items-center">
-          <img className="max-lg:hidden" src={containerhorizon} />
-          <img className="w-full h-full lg:hidden" src={containervertical} />
-          <div className="absolute lg:mb-[100px] text-center text-white text-[46px] font-extrabold leading-[57.50px] font-['Nunito'] max-lg:left-0 max-lg:top-[120px]">
-            Let’s start <br className="xl:hidden" /> finding <br /> and matching
-            someone new
+      <section className=" bg-[#160404] lg:px-40 lg:pt-20 lg:pb-[120px]">
+        <div className=" relative flex justify-center items-center lg:rounded-[32px]">
+          <img
+            className=" absolute w-full h-full max-lg:hidden object-cover z-0 rounded-[32px]"
+            src={containerhorizon}
+          />
+          <img
+            className=" absolute w-full h-full lg:hidden object-cover z-0"
+            src={containervertical}
+          />
+          <div className=" flex flex-col gap-7 lg:gap-10 items-center z-10 py-32 lg:pt-[83px] lg:pb-[81px] ">
+            <div className="text-center text-white text-[46px] font-extrabold leading-[57.50px] font-['Nunito']">
+              Let’s start <br className="xl:hidden" /> finding <br /> and
+              matching someone new
+            </div>
+            <button className="px-6 py-3 bg-rose-100 rounded-full font-bold leading-normal font-['Nunito'] text-red-600">
+              Start Matching!
+            </button>
           </div>
-          <button className="px-6 py-3 bg-rose-100 rounded-full font-bold leading-normal font-['Nunito'] text-rose-800 absolute top-[230px] max-lg:left-[120px] max-lg:top-[400px]">
-            Start Matching!
-          </button>
         </div>
       </section>
       {/* <div className="max-lg:absolute max-lg:top-[1200px]"> */}
-      <div className="max-lg:absolute max-lg:top-[3999px]">
+      <div className="">
         <Footer />
       </div>
     </div>
