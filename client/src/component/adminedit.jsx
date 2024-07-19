70b49694fd822c89f00750eb47e26770bc73d1bf
Paktakraw <tresarpnat27@gmail.com>
2024-07-19 16:40:50 +0700

    fix:add name natsu on work

--- client/src/component/adminedit.jsx
+++ client/src/component/adminedit.jsx
@@ -166,7 +166,7 @@ const AdminEditPackagePage = () => {
     }));
   };
 
-  // ส่งข้อมูลใหม่ไปจัดเก็บใน data base
+  // ส่งข้อมูลใหม่ไปจัดเก็บใน data base (natsu)
   const handleSubmit = async (e) => {
     e.preventDefault();
 
