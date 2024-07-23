03f474862e667837441e58aa468749ca2c264843
Pond <pondwachi.th@gmail.com>
2024-07-23 18:02:03 +0700

    wip: work in progress config

--- client/src/routes/non-user-routes.jsx
+++ client/src/routes/non-user-routes.jsx
@@ -1,15 +1,17 @@
 import LandingPage from "../components/landing-page";
 import Login from "../components/Login";
 import RegisterPage from "../pages/non-user/register-page";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 function NonUserRoutes() {
   return (
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/register" element={<RegisterPage />} />
-      <Route path="/login" element={<Login />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
