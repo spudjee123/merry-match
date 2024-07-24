b52484e5faf5cee175e85f7b0e65293aaffc6af8
unknown <sornram.spy@gmail.com>
2024-07-24 09:10:40 +0700

    feat:add seeprofile and filter

--- client/src/components/MatchingAdvance.jsx
+++ client/src/components/MatchingAdvance.jsx
@@ -19,6 +19,7 @@ import next_icon from "../assets/icons/next-vector-icon.png";
 import location_icon from "../assets/icons/location-icon.png";
 import reject_icon from "../assets/icons/reject-icon.png";
 import love_icon from "../assets/icons/love-icon.png";
+import preview_exit_icon from "../assets/icons/preview-exit-icon.png";
 
 const db = [
   {
@@ -50,6 +51,8 @@ function MatchingAdvanced() {
   const [lastDirection, setLastDirection] = useState();
   const [isSecondImage, setIsSecondImage] = useState(false);
   const [isMobilePreview, setIsMobilePreview] = useState(false);
+  const [hobbiesList, setHobbiesList] = useState([]);
+  const [hobby, setHobby] = useState("");
   const currentIndexRef = useRef(currentIndex);
 
   const childRefs = useMemo(
@@ -124,9 +127,170 @@ function MatchingAdvanced() {
                     </p>
                   </div>
 
-                  <button className="mt-2">
+                  <button className="mt-2"></button>
+                  <button
+                    className=""
+                    onClick={() =>
+                      document.getElementById("SeeProfileMobile").showModal()
+                    }
+                  >
                     <img src={SeeProfile} className="w-[60px] h-[60px]" />
                   </button>
+                  <dialog id="SeeProfileMobile" className="modal">
+                    <div className="w-screen h-screen flex justify-center items-center">
+                      <div className="bg-white rounded-[32px] w-[80%] h-[80%] px-12 py-10 shadow-primary relative ">
+                        <div className="relative flex justify-center items-center h-full">
+                          <img
+                            style={{
+                              backgroundImage: `url(${db[currentIndex].url})`,
+                            }}
+                            className="w-[450px] h-[400px]"
+                          />
+                          <button className=" bg-dark absolute w-12 h-12 flex justify-center items-center rounded-full top-2 left-2 shadow-primary">
+                            <img
+                              src={preview_exit_icon}
+                              width={16}
+                              height={16}
+                              alt="preview exit icon"
+                              onClick={(event) => {
+                                event.preventDefault();
+                                setIsMobilePreview(false);
+                              }}
+                              className=" shadow-2xl"
+                            />
+                          </button>
+                        </div>
+                        <div className=" h-12 flex justify-between items-center absolute text-gray-700 ">
+                          <div className=" flex justify-center absolute top-[-30px] w-full gap-6 z-10">
+                            <button className=" w-[60px] h-[60px] bg-white shadow-primary rounded-2xl">
+                              <img
+                                src={reject_icon}
+                                width={40}
+                                height={40}
+                                className=" mx-auto"
+                              />
+                            </button>
+                            <button className=" w-[60px] h-[60px] bg-white shadow-primary rounded-2xl">
+                              <img
+                                src={love_icon}
+                                width={50}
+                                height={50}
+                                className=" mx-auto"
+                              />
+                            </button>
+                          </div>
+                          <p className=" w-[72px] flex justify-center items-center z-20">
+                            {isSecondImage ? "2" : "1"}
+                            <span className=" text-gray-600">/2</span>
+                          </p>
+                          <div className=" flex z-20">
+                            <button
+                              type="button"
+                              id="back-preview-image-btn"
+                              onClick={(event) => {
+                                event.preventDefault();
+                                setIsSecondImage(false);
+                              }}
+                              className=" w-12 h-12 rounded-xl flex justify-center items-center"
+                            >
+                              <img
+                                src={back_icon}
+                                width={16}
+                                height={16}
+                                alt="back photo icon"
+                              />
+                            </button>
+                            <button
+                              type="button"
+                              id="next-preview-image-btn"
+                              onClick={(event) => {
+                                event.preventDefault();
+                                setIsSecondImage(true);
+                              }}
+                              className=" w-12 h-12 rounded-xl flex justify-center items-center"
+                            >
+                              <img
+                                src={next_icon}
+                                width={16}
+                                height={16}
+                                alt="next photo icon"
+                              />
+                            </button>
+                          </div>
+                        </div>
+
+                        <div className="flex flex-col px-4 py-6 gap-6 items-center text-gray-900 leading-6">
+                          <article className=" w-full">
+                            <h1 className=" text-[46px] leading-[57.5px] font-extrabold mb-2">
+                              {"John Snow"}{" "}
+                              <span className=" text-gray-700">{26}</span>
+                            </h1>
+                            <div className=" flex gap-4">
+                              <img
+                                src={location_icon}
+                                width={24}
+                                height={24}
+                                alt="location icon"
+                              />
+                              <p className=" text-xl leading-[30px] font-semibold">
+                                {"Bangkok, Thailand"}
+                              </p>
+                            </div>
+                          </article>
+
+                          <section className=" grid grid-cols-2 w-full py-2 gap-y-4">
+                            <p className=" flex items-center">
+                              Sexual identities
+                            </p>
+                            <p className=" font-semibold text-gray-700">
+                              {"Male"}
+                            </p>
+                            <p className=" flex items-center">
+                              Sexual preferences
+                            </p>
+                            <p className=" font-semibold text-gray-700">
+                              {"Female"}
+                            </p>
+                            <p className=" flex items-center">
+                              Racial preferences
+                            </p>
+                            <p className=" font-semibold text-gray-700">
+                              {"Asian"}
+                            </p>
+                            <p className=" flex items-center">
+                              Meeting interests
+                            </p>
+                            <p className=" font-semibold text-gray-700">
+                              {"Friends"}
+                            </p>
+                          </section>
+
+                          <article className=" w-full ">
+                            <h2 className=" mb-4 text-2xl font-bold lead-[30px]">
+                              About me
+                            </h2>
+                            <p>{"I know nothing...but you"}</p>
+                          </article>
+
+                          <article className=" w-full">
+                            <h2 className=" mb-6 text-2xl font-bold lead-[30px]">
+                              Hobbies and Interests
+                            </h2>
+                            <div className=" flex flex-wrap gap-3">
+                              {hobbiesList.map((item, index) => (
+                                <div
+                                  key={index}
+                                  className=" px-4 py-2 rounded-xl border border-purple-300 text-purple-600"
+                                >
+                                  {item}
+                                </div>
+                              ))}
+                            </div>
+                          </article>
+                        </div>
+                      </div>
+                    </div>
+                  </dialog>
                 </div>
               </div>
             </div>
@@ -157,7 +321,13 @@ function MatchingAdvanced() {
                 <div className="w-[90%] h-full flex flex-row items-end justify-between">
                   <h3 className="text-white text-[25px] flex justify-center items-center">
                     <div className="">{db[currentIndex].name} 24</div>
-                    <button className="mt-2">
+                    {/* <button className="mt-2"></button> */}
+                    <button
+                      className="mt-2"
+                      onClick={() =>
+                        document.getElementById("SeeProfileDesktop").showModal()
+                      }
+                    >
                       <img src={SeeProfile} className="w-[60px] h-[60px]" />
                     </button>
                   </h3>
@@ -175,6 +345,7 @@ function MatchingAdvanced() {
           </TinderCard>
         )}
       </div>
+
       <div className="buttons flex space-x-4 mt-[-55px] z-40">
         <button
           className={`p-2 rounded ${!canSwipe ? "" : ""}`}
@@ -198,7 +369,24 @@ function MatchingAdvanced() {
       {/* bottom mobile*/}
       <div className="w-[90%] flex items-end justify-between lg:hidden">
         <p className="flex flex-row ">
-          <img src={filter} className="w-[25px] h-[25px] mr-2" />
+          <button
+            className=""
+            onClick={() => document.getElementById("Filter").showModal()}
+          >
+            <img src={filter} className="w-[25px] h-[25px] mr-2" />
+          </button>
+          <dialog id="Filter" className="modal">
+            <div className="modal-box w-11/12 max-w-5xl">
+              <h3 className="font-bold text-lg">Hello!</h3>
+              <p className="py-4">Click the button below to close</p>
+              <div className="modal-action">
+                <form method="dialog">
+                  {/* if there is a button, it will close the modal */}
+                  <button className="btn">Close</button>
+                </form>
+              </div>
+            </div>
+          </dialog>
           Filter
         </p>
         <p className=" flex flex-row">
