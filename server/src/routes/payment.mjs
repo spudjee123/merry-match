bda2c11f40b0ccf810033953cf319ddfd87df6e6
NoonMira <163841273+NoonMira@users.noreply.github.com>
2024-08-09 12:57:04 +0700

    feat: add api for payment

--- server/src/routes/payment.mjs
+++ server/src/routes/payment.mjs
@@ -3,6 +3,7 @@ import connectionPool from "../utils/db.mjs";
 import express from "express";
 import Stripe from "stripe";
 import { v4 as uuidv4 } from "uuid";
+import dotenv from 'dotenv';
 
 const app = express();
 
@@ -74,13 +75,14 @@ stripeRouter.post("/api/checkout", express.json(), async (req, res) => {
 // เช็ค order id ว่า status เป็นยังไง
 stripeRouter.get("/api/order/:id", async (req, res) => {
   const orderId = req.params.id;
+  const newOrederId = String(orderId)
   try {
     const result = await connectionPool.query(
-      `select * from payment_test where order_id = $1 `,
-      [orderId]
+      `select package_name from payment_test where order_id = $1 `,
+      [newOrederId]
     );
 
-    res.status(200).json({result });
+    res.status(200).json(result.rows[0] );
   } catch (error) {
     return res.status(500).json({
       message: "Server could not read assignment because database connection",
@@ -88,50 +90,156 @@ stripeRouter.get("/api/order/:id", async (req, res) => {
   }
 });
 
-stripeRouter.post('/webhook', express.raw({ type: 'application/json' }), (req, res) => {
-  const sig = req.headers['stripe-signature'];
-  let eventData;
-
+stripeRouter.get('/complete', async (req, res) => {
   try {
-    // สร้าง event จาก payload และ signature
-    eventData = stripe.webhooks.constructEvent(req.body, sig, endpointSecret);
-  } catch (err) {
-    console.log(`Webhook error: ${err.message}`);
-    return res.status(400).send(`Webhook error: ${err.message}`);
+    const [session, lineItems] = await Promise.all([
+      stripe.checkout.sessions.retrieve(req.query.session_id, { expand: ['payment_intent.payment_method'] }),
+      stripe.checkout.sessions.listLineItems(req.query.session_id)
+    ]);
+
+    console.log("Session:", JSON.stringify(session));
+    console.log("Line Items:", JSON.stringify(lineItems));
+
+    res.send('Your payment was successful');
+  } catch (error) {
+    console.error("Error retrieving session:", error);
+    res.status(500).send('Error retrieving payment details');
   }
+})
 
-  // จัดการกับ event ที่ได้รับ
-  switch (eventData.type) {
-    case 'checkout.session.completed':
-      const sessionCompleted = eventData.data.object;
-      
-      updateStatus(sessionCompleted.id, 'completed');
-      break;
-    case 'checkout.session.expired':
-      const sessionExpired = eventData.data.object;
-      
-      updateStatus(sessionExpired.id, 'expired');
-      break;
+stripeRouter.get('/cancel', (req, res) => {
+  res.redirect('/')
+})
 
-    default:
-      console.log(`Unhandled event type ${eventData.type}`);
+// แบบ intent
+dotenv.config();
+
+
+
+stripeRouter.post("/api/payment-intent", express.json(), async (req, res) => {
+  const { user, packageName } = req.body;
+  const orderId = uuidv4();
+
+  console.log("Received package name:", packageName.name);
+
+  // Determine the price amount based on the package name
+  const priceAmount = packageName.price
+
+  console.log("Retrieved price amount:", priceAmount);
+
+
+  if (!priceAmount) {
+    return res.status(400).json({ error: "Invalid package name." });
   }
+  
+  try {
+    // Create a new customer in Stripe if not already created
+    const customer = await stripe.customers.create({
+      name: user.name,
+      email: user.email,
+      metadata: {
+        userId: user.id,
+      },
+    });
 
-  res.json({ received: true });
+    // Create a PaymentIntent with the order amount and currency
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: priceAmount * 100, // Convert the price to the smallest unit
+      currency: 'thb',
+      customer: customer.id,
+      automatic_payment_methods: {
+        enabled: true,
+      },
+      metadata: {
+        order_id: orderId,
+        package_name: packageName.name,
+      },
+    });
+
+    // Insert the order data into the payment_test table
+    const orderData = {
+      package_name: packageName.name,
+      order_id: orderId,
+      payment_intent_id: paymentIntent.id,
+      status: 'pending', 
+      created_date: new Date(),
+    };
+    console.log(paymentIntent)
+    await connectionPool.query(
+      `INSERT INTO payment_test (package_name, order_id, status, intent_id, created_date) 
+        VALUES ($1, $2, $3, $4, $5)`,
+      [
+        orderData.package_name,
+        orderData.order_id,
+        orderData.status,
+        orderData.intent_id,
+        orderData.created_date,
+      ]
+    );
+
+    // Respond with the client secret for the PaymentIntent
+    res.status(200).json({
+      message: 'สร้าง payment intent สำเร็จ',
+      // ส่งไปหน้าบ้าน
+      clientSecret: paymentIntent.client_secret,
+    });
+
+  } catch (error) {
+    console.error("Error creating Payment Intent:", error);
+    res.status(500).json({ error: error.message });
+  }
 });
 
-function updateStatus(sessionId, status) {
-  connectionPool.query(
-    'UPDATE payment_test SET status = $1 WHERE session_id = $2',
-    [status, sessionId],
-    (error, results) => {
-      if (error) {
-        console.error('Error updating payment status:', error);
-      } else {
-        console.log(`Payment status for session ${sessionId} updated to ${status}`);
-      }
-    }
-  );
-}
+stripeRouter.put("/update/payment", express.json(), async (req, res) => {
+  try {
+    // Create a new customer in Stripe if not already created
+    const customer = await stripe.customers.create({
+      name: user.name,
+      email: user.email,
+      metadata: {
+        userId: user.id,
+      },
+    });
+
+    // Create a PaymentIntent with the order amount and currency
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: priceAmount * 100, // Convert the price to the smallest unit
+      currency: 'thb',
+      customer: customer.id,
+      automatic_payment_methods: {
+        enabled: true,
+      },
+      metadata: {
+        order_id: orderId,
+        package_name: packageName.name,
+      },
+    });
+
+    // Insert the order data into the payment_test table
+    const orderData = {
+      order_id: orderId,
+      created_date: new Date(),
+    };
+    console.log(paymentIntent)
+    await connectionPool.query(
+      `Upadte payment_test set inten_id = $1 where order_id = $2`,
+      [
+        orderData.order_id,
+        orderData.created_date,
+      ]
+    );
+
+    // Respond with the client secret for the PaymentIntent
+    res.status(200).json({
+      message: 'update payment intent สำเร็จ',
+      // ส่งไปหน้าบ้าน
+      clientSecret: paymentIntent.client_secret,
+    });
+
+  } catch (error) {
+    console.error("Error creating Payment Intent:", error);
+    res.status(500).json({ error: error.message });
+  }
+});
 
 export default stripeRouter;
